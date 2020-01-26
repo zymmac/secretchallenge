fd00a1a979b29aba3968fea6df61aa9966e6b994
ZymmAc <zymmac@gmail.com>
2020-01-26 10:57:23 -0300

    toggle active class navbar

--- pool.js
+++ pool.js
@@ -1,1 +1,9 @@
-console.log('connected');
+// console.log('connected');
+$(function() {
+   $(".tab").click(function() {
+      // remove classes from all
+      $(".tab").removeClass("active");
+      // add class to the one we clicked
+      $(this).addClass("active");
+   });
+});
