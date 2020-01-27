5fce213cbe2e5bdfd10fdab3ab11f6351a74a404
Alexandre Mendes Costa <alexandre.costa@anac.gov.br>
2020-01-27 14:29:58 -0300

    Show/Hide Forms (Rectangular & Round)

--- pool.js
+++ pool.js
@@ -1,4 +1,6 @@
-// console.log('connected');
+/////////// Functions
+
+// Change Active Tab
 $(function() {
    $(".tab").click(function() {
       // remove classes from all
@@ -7,3 +9,21 @@ $(function() {
       $(this).addClass("active");
    });
 });
+
+/////// Show/Hide Form Inputs
+$(function() {
+  activeRound();
+})
+// Show/Hide Rectangular
+function activeRectangular() {
+$('.form-group').css("display","none");
+$('.form-group.Rectangular').css("display","block");
+$('.form-group.Depth').css("display","block");
+}
+
+// Show/Hide Round
+function activeRound() {
+$('.form-group').css("display","none");
+$('.form-group.Round').css("display","block");
+$('.form-group.Depth').css("display","block");
+}
